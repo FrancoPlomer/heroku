cbe478f796e8af2d1cc6ee1b24d1e8dd968a3cd7
PlomerFranco <franko.plomer@gmail.com>
2022-05-10 21:09:26 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola desde heroku');
+})
+
+const server = app.listen(port, () => {
+    console.log(`Escuchando puerto ${port}`)
+})
+
+server.on(error => {
+    console.log("Error: ", error)
+})
